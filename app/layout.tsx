6d966a1e59924e141eef29613e53e945198408ce
DanielHe09 <dandanhe09@gmail.com>
2026-03-29 16:27:07 -0400

    fixed favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,8 +11,8 @@ export const metadata: Metadata = {
   description: 'Created with v0',
   generator: 'v0.app',
   icons: {
-    icon: '/Favicon.png?v=2',
-    apple: '/Favicon.png?v=2',
+    icon: '/favicon.png',
+    apple: '/favicon.png',
   },
 }
 
